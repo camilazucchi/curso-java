50e19c1f3af0017890a4ca15426d4f1be74f3e52
camilazci <camilazucchi@outlook.com>
2025-01-29 14:12:19 -0300

    feat(example): add constructors to Produto class

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -6,5 +6,15 @@ public class Produto {
     double preco;
     double desconto;
 
+    // Construtor padrão de forma explícita
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+    double precoComDesconto() {
+        return preco * (1 - desconto);
+    }
 
 }
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -4,26 +4,19 @@ package classe;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
-        p1.preco = 4356.89;
-        p1.desconto = 0.25; // 25%
-
-        var p2 = new Produto();
-        p2.nome = "Caneta Azul";
-        p2.preco = 12.56;
-        p2.desconto = 0.29;
+        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+        Produto p2 = new Produto("Caneta Azul", 12.56, 0.29);
 
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        // Refatoração utilizando o método 'precoComDesconto':
+        double precoFinal1 = p1.precoComDesconto();
+        double precoFinal2 = p2.precoComDesconto();
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
-
-
+        System.out.println();
 
     }
 }
