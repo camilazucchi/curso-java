258d2861cdbfd59660d235aba1782ffa74a6dee3
camilazci <camilazucchi@outlook.com>
2025-01-29 15:58:02 -0300

    feat(exercise): add info to products

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -4,8 +4,8 @@ package classe;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
-        Produto p2 = new Produto("Caneta Azul", 12.56, 0.29);
+        Produto p1 = new Produto("Notebook", 4356.89);
+        Produto p2 = new Produto("Caneta Azul", 12.56);
 
         System.out.println(p1.nome);
         System.out.println(p2.nome);
@@ -15,6 +15,10 @@ public class ProdutoTeste {
         double precoFinal2 = p2.precoComDesconto();
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
+        System.out.printf("- Produto: %s%n-Preço: %.2f%n", p1.nome, p1.preco);
+        System.out.printf("Preço com desconto do produto %s: %.2f%n", p1.nome, precoFinal1);
+        System.out.printf("- Produto: %s%n- Preço: %.2f%n", p2.nome, p2.preco);
+        System.out.printf("Preço com desconto do produto %s: %.2f%n", p2.nome, precoFinal2);
         System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
         System.out.println();
 
