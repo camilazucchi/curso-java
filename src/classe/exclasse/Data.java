ea5440898e7ba9374eff0733a73ffc914d62ffb1
camilazci <camilazucchi@outlook.com>
2025-01-29 14:12:00 -0300

    feat(exercise): add constructors and formatted date method to Data class

--- src/classe/exclasse/Data.java
+++ src/classe/exclasse/Data.java
@@ -2,9 +2,29 @@ package classe.exclasse;
 
 public class Data {
 
-    // Criar classe Data com três atributos: dia, mês e ano
+    // Desafio 1: Criar classe Data com três atributos: dia, mês e ano
     int dia;
     int mes;
     int ano;
+
+    // Desafio 3: Criar construtor padrão sem parâmetros, mas com valores definidos
+    public Data() {
+        // Dados inseridos quando não se passa nenhum dado ao instanciar um objeto Data
+        this.dia = 1;
+        this.mes = 1;
+        this.ano = 1970;
+    }
+
+    // Desafio 4: Criar construtor com parâmetros dia, mês e ano
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    // Desafio 2: Criar o método 'obterDataFormatada'
+    String obterDataFormatada() {
+        return String.format("%d/%d/%d", dia, mes, ano);
+    }
 
 }
--- src/classe/exclasse/DataTeste.java
+++ src/classe/exclasse/DataTeste.java
@@ -6,17 +6,10 @@ public class DataTeste {
         // Instanciar duas datas
 
         Data data1 = new Data();
-        data1.dia = 01;
-        data1.mes = 10;
-        data1.ano = 1933;
 
-        Data data2 = new Data();
-        data2.dia = 10;
-        data2.mes = 11;
-        data2.ano = 1997;
+        Data data2 = new Data(10, 11, 1997);
 
-        System.out.printf("Data 1\nDia: %d\nMês: %d\nAno: %d\n", data1.dia, data1.mes, data1.ano);
-        System.out.printf("\nData 2\nDia: %d\nMês: %d\nAno: %d\n", data2.dia, data2.mes, data2.ano);
-
+        System.out.println(data1.obterDataFormatada());
+        System.out.println(data2.obterDataFormatada());
     }
 }
